696c739f804801d847c44e52f97fb3895f4feed4
Ayrles Sales <ayrlesalespuc@gmail.com>
2023-05-10 23:44:11 -0300

    teste01

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const robotron = document.querySelector('#robotron')
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", dizOi)
+somar.addEventListener("click", () => {
+    braco.value = parseInt(braco.value) + 1;
+})
 
-function dizOi(nome) {
-    console.log(nome)
-    console.log("oi")
-}
-
-dizOi("ayrles")
+subtrair.addEventListener("click", () => {
+    braco.value = parseInt(braco.value) - 1;
+})
