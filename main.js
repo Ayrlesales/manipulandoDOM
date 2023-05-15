2cbd5f3d4f2b120a43985ad7fc4944cc84b512f1
Ayrles Sales <ayrlesalespuc@gmail.com>
2023-05-15 15:17:53 -0300

    utilizando datta-attributes

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const controles = document.querySelectorAll(".controle-ajuste")
+const controles = document.querySelectorAll("[data-controle]")
 
 controles.forEach( (controle) => {
     controle.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
     })
 })
 
 
 function manipulaDados(calculo, pai) {
-    const contador = pai.querySelector(".controle-contador")
+    const contador = pai.querySelector("[data-contador]")
 
     if(calculo === "+") {
         contador.value = parseInt(contador.value) + 1;
