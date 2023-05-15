03e2221d264c81b603b47e527f541852ed125c9f
Ayrles Sales <ayrlesalespuc@gmail.com>
2023-05-15 13:19:08 -0300

    usando forEach

--- main.js
+++ main.js
@@ -2,10 +2,37 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
-somar.addEventListener("click", () => {
-    braco.value = parseInt(braco.value) + 1;
+
+//parseInt transforma valores(string) em número
+//somar.addEventListener("click", () => {
+ //   braco.value = parseInt(braco.value) + 1;
+//})
+
+//subtrair.addEventListener("click", () => {
+ //   braco.value = parseInt(braco.value) - 1;
+//})
+
+//somar.addEventListener("click", () => {
+//    manipulaDados("somar")
+//})
+
+//subtrair.addEventListener("click", () => {
+//    manipulaDados("subtrair")
+//})
+
+const controles = document.querySelectorAll(".controle-ajuste")
+
+controles.forEach( (controle) => {
+    controle.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
 })
 
-subtrair.addEventListener("click", () => {
-    braco.value = parseInt(braco.value) - 1;
-})
+
+function manipulaDados(calculo) {
+    if(calculo === "+") {
+        braco.value = parseInt(braco.value) + 1;
+    } else {
+        braco.value = parseInt(braco.value) - 1;
+    }
+}
